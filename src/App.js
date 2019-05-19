c819aff71e46466ac1984b6694a42b620ea1800c
Sang Dang <sangdth@gmail.com>
2019-05-19 16:33:03 +0300

    remove dist folder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            EDIT HAH <code>src/App.js</code> and save to reload.
+            EDIT <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
