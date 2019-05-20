eca64ea988b3b36c8b1728ef1474eb4df4c0bd6e
Sang Dang <sangdth@gmail.com>
2019-05-20 22:16:22 +0300

    wip login

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-// import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+import Login from '../views/login';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <Typography component="h2" variant="h1" gutterBottom>
-                h1. Heading
-            </Typography>
-        <p>
-            EDIT <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-      </a>
-      <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Login />
+        </div>
+    );
 }
 
 export default App;
